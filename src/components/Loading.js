d1707c182a38fdcbde5ee8f4d7421756247b511a
aditya2145 <148987683+aditya2145@users.noreply.github.com>
2024-12-05 11:30:20 +0530

    Update Loading.js

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react'
 
 const Loading = () => {
-  return <h2>Loading...</h2>;
-};
+  return (
+    <div className='loading' style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }}>Loading</div>
+  )
+}
 
-export default Loading;
+export default Loading
